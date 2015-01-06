9bf2da69150387adf46437523ec91f3a0c5ad98f
F Lengyel <florian.lengyel@gmail.com>
2015-01-06 17:42:38 -0500

    Create mathjaxconfig.js
    MathJax configuration object

---
+++ mathjaxconfig.js
@@ -0,0 +1,24 @@
+MathJax.Hub.Config({
+    TeX: {
+      equationNumbers: { autonumber: &quot;AMS&quot; },
+      Macros: {
+        DL:   &quot;{\\mathrm{DL}}&quot;,
+        JL:   &quot;{\\mathrm{JL}}&quot;,
+		    LoP:  &quot;{\\mathrm{LP}}&quot;,
+        DLP:  &quot;{\\DL\\oplus_{\\JL}\\LoP}&quot;,
+        PROVES:  &quot;{\\,\\vdash\\,}&quot;,
+		    MODELS:  &quot;{\\,\\vDash\\,}&quot;,
+		    AND:  &quot;{\\,\\wedge\\,}&quot;,
+		    OR:  &quot;{\\,\\vee\\,}&quot;,
+        IMP:  &quot;{\\,\\rightarrow\\,}&quot;,
+        NOT:  &quot;{\\lnot}&quot;,
+        FALSE:  &quot;{\\bot}&quot;,
+        JUST: [&quot;{{#1}:{#2}}&quot;,2],
+	      WITH: &quot;{\\,\\mathop{\\&amp;}\\,}&quot;,
+        HASH: &quot;{\\,\\boxtimes\\,}&quot;,
+        OK:   [&quot;{\\mathrm{OK}\\left({#1}\\right)}&quot;,1],
+        STAR: [&quot;{{#1}^*}&quot;,1]
+      }
+    }
+});
+
