04569a93227b7d2cb76aa62eae5a21664737fa35
F Lengyel <flengyel@users.noreply.github.com>
2016-02-08 00:28:02 -0500

    Update mathjaxconfig.js
    Macro for real numbers added.

--- mathjaxconfig.js
+++ mathjaxconfig.js
@@ -2,6 +2,7 @@ MathJax.Hub.Config({
     TeX: {
       equationNumbers: { autoNumber: "AMS" },
       Macros: {
+      	R:    "{\\mathbb{R}}",
     	CL:   "{\\mathrm{CL}}",
         DL:   "{\\mathrm{DL}}",
         JL:   "{\\mathrm{JL}}",
