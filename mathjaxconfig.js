ec1af0cb73fc17bc60b4cc9995c298bb8f9427a1
F Lengyel <flengyel@users.noreply.github.com>
2017-12-31 12:59:27 -0500

    http changed to https

--- mathjaxconfig.js
+++ mathjaxconfig.js
@@ -26,5 +26,5 @@ MathJax.Hub.Config({
       }
     }
 });
-MathJax.Ajax.loadComplete("http://flengyel.github.io/mathjaxconfig.js");
+MathJax.Ajax.loadComplete("https://flengyel.github.io/mathjaxconfig.js");
 
